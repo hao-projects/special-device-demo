473a1bd8f2453a2892bc3c7a7436476553514a9d
hao-projects <652911031@qq.com>
2018-05-18 23:12:44 +0800

    fix redis bugs & add cache annotation

--- src/main/java/com/example/demo/Consts/ExcelTitle.java
+++ src/main/java/com/example/demo/Consts/ExcelTitle.java
@@ -8,15 +8,39 @@ import java.util.List;
 public final class ExcelTitle {
     public static List<Pair<Integer,String>> getTotalDeviceFormTitle(){
         return Lists.newArrayList(
-                Pair.of(0, ""),
-                Pair.of(1,""),
-                Pair.of(2, ""),
-                Pair.of(3,""),
-                Pair.of(4, ""),
-                Pair.of(5,""),
-                Pair.of(6, ""),
-                Pair.of(7,"")
-
+                Pair.of(0, "#"),
+                Pair.of(1,"设备种类"),
+                Pair.of(2, "设备类别"),
+                Pair.of(3,"设备品种"),
+                Pair.of(4, "设备代码"),
+                Pair.of(5,"单位内编号"),
+                Pair.of(6, "停用开始日期"),
+                Pair.of(7,"停用结束日期"),
+                Pair.of(8,"注销日期"),
+                Pair.of(9, "使用单位填表人员"),
+                Pair.of(10,"登记机关"),
+                Pair.of(11, "申请日期"),
+                Pair.of(12,"发证日期"),
+                Pair.of(13,"使用等级证编号")
                 );
+    }
+
+    public static List<Pair<Integer, String>> getTotalRegistDetailForm(){
+        return Lists.newArrayList(
+                Pair.of(0, "#"),
+                Pair.of(1,"使用单位名称"),
+                Pair.of(2, "设备种类"),
+                Pair.of(3,"设备类别"),
+                Pair.of(4, "设备品种"),
+                Pair.of(5,"设备代码"),
+                Pair.of(6, "登记类别"),
+                Pair.of(7,"停用开始日期"),
+                Pair.of(8,"停用结束日期"),
+                Pair.of(9,"注销日期"),
+                Pair.of(10,"登记机关/人员"),
+                Pair.of(11, "申请日期"),
+                Pair.of(12,"发证日期"),
+                Pair.of(13,"使用等级证编号")
+        );
     }
 }
